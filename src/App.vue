c9e0fd17698118cc27bf008af7af465650c1745d
Jay Billings <jbillings@desc.org>
2021-11-22 17:05:51 -0800

    Fills out page; trying to work w/ postmessage

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,31 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<!--  <h1>CHASERS Vue Test</h1>-->
+
+<!--  <ActionButton label="Download" />
+  <ActionButton label="Save" />-->
+
+  <DataContainer :data="records" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import DataContainer from './components/DataContainer.vue'
+//import ActionButton from './components/ActionButton.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    DataContainer//, ActionButton
+  },
+  data() {
+    return {
+      records: [
+        {title: "Record Number One", content: "Lines unto felt native charms did den some me. A from she stalked for or, he and was revel dwell."},
+        {title: "Record Number Two", content: "Harold childe he where present the childe childe. The like almost felt this feud companie, cheer formed to this worse."},
+        {title: "Record Number Three", content: "The breast since childe wassailers me, mine satiety begun not venerable deemed earthly the to, save flatterers might him say."},
+        {title: "Record Number Four", content: "Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere."},
+        {title: "Record Number Five", content: "Nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur,"}
+      ]
+    }
   }
 }
 </script>
@@ -21,6 +37,7 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 2rem 2rem 1rem;
+  font-size: 16px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,25 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import "./styles/main.css"
+
+document.addEventListener('message', e => {
+    // TODO: Put this in the module lifecycle
+    console.log('got a message from parent')
+    if (e.origin !== 'http://localhost') {
+        console.error(`Received a message from an unexpected source: ${e.origin}`);
+        return;
+    }
+
+    let [event] = e.data;
+
+    console.log(`Received ${e.data} from ${e.origin}`)
+    if (event === 'parentReady') {
+        const myHeight = document.body.clientHeight,
+            parentWindow = window.parent;
+
+        console.log(`im iframe and my height is ${myHeight}`);
+        parentWindow.postMessage(['setIframeHeight', myHeight], '*');
+    }
+})
 
 createApp(App).mount('#app')
